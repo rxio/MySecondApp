0f0b38a9cc332d427817e8998490715b49c4e7df
rxio <xiongrichard@gmail.com>
2018-09-26 23:57:46 -0400

    Added some new stuff

--- app/src/main/java/com/example/richard/mysecondapp/MainActivity.java
+++ app/src/main/java/com/example/richard/mysecondapp/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.richard.mysecondapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.richard.mysecondapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +18,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void sendMessage(View view) {
         //Do something in response to the button
-
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
